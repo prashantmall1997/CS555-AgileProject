18ad4465aec4ae5808523c5cc5d15f2a4786f465
Mrunal Salunke <67240029+mrunal2021@users.noreply.github.com>
2021-03-15 12:47:21 -0400

    Modify gedcom file and implement user stories 7 and 8

--- gedcomParser.js
+++ gedcomParser.js
@@ -91,6 +91,28 @@ function dateChecker(date1, date2 = new Date()) {
   }
 }
 
+//Age Calculator Function
+function calculateAge(dob, dod) {
+  //dob- date of birth, dod- date of death
+  let birthDay, current, millisec_difference, deathDay;
+
+  birthDay = new Date(Date.parse(dob));
+  deathDay = new Date(Date.parse(dod));
+  //console.log(deathDay);
+
+  current = new Date();
+
+  if (!dod) {
+    millisec_difference = Math.abs(current.getTime() - birthDay.getTime());
+  } else {
+    millisec_difference = Math.abs(deathDay.getTime() - birthDay.getTime());
+  }
+
+  let age = millisec_difference / (1000 * 3600 * 24 * 365);
+
+  return age;
+}
+
 //Errors Array to store all error in gedcom file
 var errors = [];
 
@@ -284,6 +306,82 @@ for (let line = 0; line < data.length; line++) {
 }
 console.log("Families");
 console.table(familyData);
+
+//US07 - Less then 150 years old	- Death should be less than 150 years after birth for dead people, and current date should be less than 150 years after birth for all living people
+for (
+  indiDataElement = 0;
+  indiDataElement < individualData.length;
+  indiDataElement++
+) {
+  if (individualData[indiDataElement].Alive == true) {
+    let age = calculateAge(individualData[indiDataElement].Birthday);
+
+    if (age > 150) {
+      errors.push(
+        `ERROR: INDIVIDUAL: US07: ${individualData[indiDataElement].ID}: More than 150 years old - Birth ${individualData[indiDataElement].Birthday}`
+      );
+    }
+  } else {
+    let age = calculateAge(
+      individualData[indiDataElement].Birthday,
+      individualData[indiDataElement].Death
+    );
+
+    if (age > 150) {
+      errors.push(
+        `ERROR: INDIVIDUAL: US07: ${individualData[indiDataElement].ID}: More than 150 years old at death - Birth ${individualData[indiDataElement].Birthday} : Death ${individualData[indiDataElement].Death}`
+      );
+    }
+  }
+}
+
+//US08	Birth before marriage of parents	Children should be born after marriage of parents (and not more than 9 months after their divorce)
+for (
+  familyDataElement = 0;
+  familyDataElement < familyData.length;
+  familyDataElement++
+) {
+  if (familyData[familyDataElement].Children.length != 0) {
+    for (
+      childrenDataElement = 0;
+      childrenDataElement < familyData[familyDataElement].Children.length;
+      childrenDataElement++
+    ) {
+      for (
+        indiDataElement = 0;
+        indiDataElement < individualData.length;
+        indiDataElement++
+      ) {
+        if (
+          familyData[familyDataElement].Children[childrenDataElement] ==
+          individualData[indiDataElement].ID
+        ) {
+          if (
+            dateChecker(
+              individualData[indiDataElement].Birthday,
+              familyData[familyDataElement].Married
+            )
+          ) {
+            errors.push(
+              `ANAMOLY: FAMILY: US08: ${familyData[familyDataElement].ID} Child ${individualData[indiDataElement].ID} born ${individualData[indiDataElement].Birthday} before marriage on ${familyData[familyDataElement].Married}`
+            );
+          } else if (familyData[familyDataElement].Divorced != "NA") {
+            let difference = calculateAge(
+              familyData[familyDataElement].Divorced,
+              individualData[indiDataElement].Birthday
+            );
+
+            if (difference >= 0.75) {
+              errors.push(
+                `ANAMOLY: FAMILY: US08: ${familyData[familyDataElement].ID} Child ${individualData[indiDataElement].ID} born ${individualData[indiDataElement].Birthday} after divorce on ${familyData[familyDataElement].Divorced}`
+              );
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
 //Printing errors in GEDCOM file
 for (error in errors) {
