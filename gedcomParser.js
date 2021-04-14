473c1aaa5b57644410efca1a6c3e1d1d01e2fe63
prashantmal1997 <prashantmall1997@gmail.com>
2021-04-14 18:11:56 -0400

    US 1,2,11,12,25&27 Test Case

--- gedcomParser.js
+++ gedcomParser.js
@@ -202,6 +202,373 @@ function calculateAge(dob, dod) {
   return age;
 }
 
+//US01 - Dates before current date
+function US01(fileName) {
+  var data = parseGedcom(fileName);
+  var individualData = data.individualData;
+  var familyData = data.familyData;
+  var noError = true;
+
+  for (
+    indiDataElement = 0;
+    indiDataElement < individualData.length;
+    indiDataElement++
+  ) {
+    if (!dateChecker(individualData[indiDataElement].Birthday)) {
+      errors.push(
+        `ERROR: INDIVIDUAL: US01: ${individualData[indiDataElement].ID}: Birthday ${individualData[indiDataElement].Birthday} occurs in the future.`
+      );
+      noError = false;
+    }
+    if (individualData[indiDataElement].Death != "NA") {
+      if (!dateChecker(individualData[indiDataElement].Death)) {
+        errors.push(
+          `ERROR: INDIVIDUAL: US01: ${individualData[indiDataElement].ID}: Death ${individualData[indiDataElement].Death} occurs in the future.`
+        );
+        noError = false;
+      }
+    }
+  }
+
+  for (
+    familyDataElement = 0;
+    familyDataElement < familyData.length;
+    familyDataElement++
+  ) {
+    if (!dateChecker(familyData[familyDataElement].Married)) {
+      errors.push(
+        `ERROR: INDIVIDUAL: US01: ${familyData[familyDataElement].ID}: Marriage ${familyData[familyDataElement].Married} occurs in the future.`
+      );
+      noError = false;
+    }
+    if (familyData[familyDataElement].Divorced != "NA") {
+      if (!dateChecker(familyData[familyDataElement].Divorced)) {
+        errors.push(
+          `ERROR: FAMILY: US01: ${familyData[familyDataElement].ID}: Divorce ${familyData[familyDataElement].Divorced} occurs in the future.`
+        );
+        noError = false;
+      }
+    }
+  }
+
+  return noError;
+}
+US01(gedcomFileName);
+
+//US11 Marriage should not occur during marriage to another spouse
+function US11(fileName) {
+  var data = parseGedcom(fileName);
+  var individualData = data.individualData;
+  var familyData = data.familyData;
+  var noError = true;
+
+  for (let line = 0; line < data.length; line++) {
+    var lineData = data[line].split(" ");
+
+    if (lineData[2] === "FAM") {
+      for (let counter = line + 1; counter < data.length; counter++) {
+        var familyLineData = data[counter].split(" ");
+
+        if (familyLineData[1] === "MARR") {
+          marriageDate = data[counter + 1]
+            .split(" ")
+            .slice(2, data[counter + 1].length)
+            .join(" ");
+
+          for (
+            let marriageCounter = counter + 1;
+            marriageCounter < data.length;
+            marriageCounter++
+          ) {
+            var familyLineData = data[marriageCounter].split(" ");
+
+            if (familyLineData[1] === "MARR") {
+              compareMarriageDate = data[marriageCounter + 1]
+                .split(" ")
+                .slice(2, data[marriageCounter + 1].length)
+                .join(" ");
+
+              if (marriageDate === compareMarriageDate) {
+                var famOneIncrement;
+                var famTwoIncrement;
+                if (data[counter + 2].split(" ")[1] === "HUSB") {
+                  famOneIncrement = 2;
+                } else {
+                  famOneIncrement = 4;
+                }
+                if (data[marriageCounter + 2].split(" ")[1] === "HUSB") {
+                  famTwoIncrement = 2;
+                } else {
+                  famTwoIncrement = 4;
+                }
+
+                if (
+                  data[counter + famOneIncrement].split(" ")[2] ===
+                  data[marriageCounter + famTwoIncrement].split(" ")[2]
+                ) {
+                  errors.push(
+                    `ANAMOLY: FAMILY: US11: ${data[marriageCounter - 1]
+                      .split(" ")[1]
+                      .replace(/[@]/g, "")}: Marriage of Husband ${data[
+                      marriageCounter + famTwoIncrement
+                    ]
+                      .split(" ")[2]
+                      .replace(
+                        /[@]/g,
+                        ""
+                      )} occurs with both the spouse on same day.`
+                  );
+                  counter = marriageCounter;
+                  noError = false;
+                  break;
+                }
+                if (
+                  data[counter + famOneIncrement + 1].split(" ")[2] ===
+                  data[marriageCounter + famTwoIncrement + 1].split(" ")[2]
+                ) {
+                  errors.push(
+                    `ANAMOLY: FAMILY: US11: ${data[marriageCounter - 1]
+                      .split(" ")[1]
+                      .replace(/[@]/g, "")}: Marriage of Wife ${data[
+                      marriageCounter + famTwoIncrement + 1
+                    ]
+                      .split(" ")[2]
+                      .replace(
+                        /[@]/g,
+                        ""
+                      )} occurs with both the spouse on same day.`
+                  );
+                  counter = marriageCounter;
+                  noError = false;
+                  break;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return noError;
+}
+US11(gedcomFileName);
+
+//US12 Mother should be less than 60 years older than her children and father should be less than 80 years older than his children
+function US12(fileName) {
+  var data = parseGedcom(fileName);
+  var individualData = data.individualData;
+  var familyData = data.familyData;
+  var noError = true;
+
+  for (let line = 0; line < data.length; line++) {
+    var lineData = data[line].split(" ");
+    if (lineData[1] === "HUSB") {
+      var husband = lineData[2];
+      var wife = data[line + 1].split(" ")[2];
+      for (let counter = line + 1; counter < data.length; counter++) {
+        var familyLineData = data[counter].split(" ");
+        if (familyLineData[2] === "FAM") {
+          break;
+        }
+
+        for (let lineLoop = 0; lineLoop < data.length; lineLoop++) {
+          var lineData = data[lineLoop].split(" ");
+
+          if (lineData[1] === husband) {
+            for (
+              let counterLoop = lineLoop + 1;
+              counterLoop < data.length;
+              counterLoop++
+            ) {
+              var nameLineData = data[counterLoop].split(" ");
+
+              if (nameLineData[1] === "BIRT") {
+                husbandBirthday = data[counterLoop + 1]
+                  .split(" ")
+                  .slice(2, data[counterLoop + 1].length)
+                  .join(" ");
+                break;
+              }
+            }
+          }
+        }
+
+        for (let lineLoop = 0; lineLoop < data.length; lineLoop++) {
+          var lineData = data[lineLoop].split(" ");
+
+          if (lineData[1] === wife) {
+            for (
+              let counterLoop = lineLoop + 1;
+              counterLoop < data.length;
+              counterLoop++
+            ) {
+              var nameLineData = data[counterLoop].split(" ");
+
+              if (nameLineData[1] === "BIRT") {
+                wifeBirthday = data[counterLoop + 1]
+                  .split(" ")
+                  .slice(2, data[counterLoop + 1].length)
+                  .join(" ");
+                break;
+              }
+            }
+          }
+        }
+
+        if (familyLineData[1] === "CHIL") {
+          var child = familyLineData[2];
+
+          for (let lineLoop = 0; lineLoop < data.length; lineLoop++) {
+            var lineData = data[lineLoop].split(" ");
+
+            if (lineData[1] === child) {
+              for (
+                let counterLoop = lineLoop + 1;
+                counterLoop < data.length;
+                counterLoop++
+              ) {
+                var nameLineData = data[counterLoop].split(" ");
+
+                if (nameLineData[1] === "BIRT") {
+                  childBirthday = data[counterLoop + 1]
+                    .split(" ")
+                    .slice(2, data[counterLoop + 1].length)
+                    .join(" ");
+                  break;
+                }
+              }
+            }
+          }
+
+          var childDate = new Date(Date.parse(childBirthday));
+          var husbandDate = new Date(Date.parse(husbandBirthday));
+          var wifeDate = new Date(Date.parse(wifeBirthday));
+
+          var currDate = new Date();
+
+          var childAge = Math.floor(
+            (currDate.getTime() - childDate.getTime()) / 31557600000
+          );
+          var husbandAge = Math.floor(
+            (currDate.getTime() - husbandDate.getTime()) / 31557600000
+          );
+          var wifeAge = Math.floor(
+            (currDate.getTime() - wifeDate.getTime()) / 31557600000
+          );
+
+          var famOneIncrement;
+          if (data[line - 3].split(" ")[2] === "FAM") {
+            famOneIncrement = 3;
+          } else {
+            famOneIncrement = 5;
+          }
+
+          if (wifeAge - childAge >= 60) {
+            errors.push(
+              `ERROR: FAMILY: US12: ${data[line - famOneIncrement]
+                .split(" ")[1]
+                .replace(/[@]/g, "")}: Age of Mother ${wife.replace(
+                /[@]/g,
+                ""
+              )} should be less than 60 years older than her children.`
+            );
+            noError = false;
+          }
+
+          if (husbandAge - childAge >= 80) {
+            errors.push(
+              `ERROR: FAMILY: US12: ${data[line - famOneIncrement]
+                .split(" ")[1]
+                .replace(/[@]/g, "")}: Age of Father ${husband.replace(
+                /[@]/g,
+                ""
+              )} should be less than 80 years older than his children.`
+            );
+            noError = false;
+          }
+
+          // console.log(
+          //   `Child ${child} age: ${childAge} of Father ${husband} Age: ${husbandAge} & Mother ${wife} Age: ${wifeAge}`
+          // );
+        }
+      }
+    }
+  }
+  return noError;
+}
+US12(gedcomFileName);
+
+//US25 Unique first names in families
+function US25(fileName) {
+  var data = parseGedcom(fileName);
+  var individualData = data.individualData;
+  var familyData = data.familyData;
+  var noError = true;
+
+  for (var family = 0; family < familyData.length; family++) {
+    const childrenData = [];
+    for (
+      var children = 0;
+      children < familyData[family].Children.length;
+      children++
+    ) {
+      const child = familyData[family].Children[children];
+
+      for (var people = 0; people < individualData.length; people++) {
+        if (child === individualData[people].ID) {
+          var childDOB = individualData[people].Birthday;
+          var childName = individualData[people].Name;
+          break;
+        }
+      }
+      childrenData.push({
+        ID: child,
+        FID: familyData[family].ID,
+        Name: childName,
+        Birthday: childDOB,
+      });
+    }
+    for (var loop = 0; loop < childrenData.length; loop++) {
+      for (
+        var innerLoop = loop + 1;
+        innerLoop < childrenData.length;
+        innerLoop++
+      ) {
+        if (
+          childrenData[loop].Name === childrenData[innerLoop].Name &&
+          childrenData[loop].Birthday === childrenData[innerLoop].Birthday
+        ) {
+          errors.push(
+            `ERROR: FAMILY: US25: Name and DOB of ${childrenData[loop].ID} & ${childrenData[innerLoop].ID} are same in family ${childrenData[innerLoop].FID}`
+          );
+          noError = false;
+        }
+      }
+    }
+  }
+  return noError;
+}
+US25(gedcomFileName);
+
+//US27 Include individual ages
+function US27(fileName) {
+  var data = parseGedcom(fileName);
+  var individualData = data.individualData;
+  var familyData = data.familyData;
+  var noError = true;
+
+  for (var people = 0; people < individualData.length; people++) {
+    if (isNaN(individualData[people].Age)) {
+      errors.push(
+        `ERROR: ERROR: US27: Age of ${individualData[people].ID} is not included`
+      );
+      noError = false;
+    }
+  }
+  return noError;
+}
+US27(gedcomFileName);
+
 var data = parseGedcom(gedcomFileName);
 var individualData = data.individualData;
 var familyData = data.familyData;
@@ -217,4 +584,9 @@ for (error in errors) {
 
 module.exports = {
   parseGedcom,
+  US01,
+  US11,
+  US12,
+  US25,
+  US27,
 };
--- gedcomParser.test.js
+++ gedcomParser.test.js
@@ -1,5 +1,29 @@
-const { parseGedcom, US01 } = require("./gedcomParser");
+const { parseGedcom, US01, US11, US12, US25, US27 } = require("./gedcomParser");
 
 test("Testing Base Case", () => {
   expect(parseGedcom("gedcomTestDataBase").noError).toBe(true);
+});
+
+test("Testing Base Case", () => {
+  expect(parseGedcom("gedcomTestDataBase").noError).toBe(true);
+});
+
+test("Test US01", () => {
+  expect(US01("gedcomTestData_US01")).toBe(false);
+});
+
+test("Test US11", () => {
+  expect(US11("gedcomTestData_US11")).toBe(false);
+});
+
+test("Test US12", () => {
+  expect(US12("gedcomTestData_US12")).toBe(false);
+});
+
+test("Test US25", () => {
+  expect(US25("")).toBe(false);
+});
+
+test("Test US27", () => {
+  expect(US27("")).toBe(false);
 });
