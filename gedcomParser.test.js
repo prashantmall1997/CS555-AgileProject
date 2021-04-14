26b3aab4d86c3f47f7dd0af0415a7d2dbd711d46
Seema <muchandiseema11@gmail.com>
2021-04-14 19:06:49 -0400

    Implement Test case 3,4 & 13,14

--- gedcomParser.test.js
+++ gedcomParser.test.js
@@ -1,4 +1,17 @@
-const { parseGedcom, US01, US11, US12, US25, US27 } = require("./gedcomParser");
+const {
+  parseGedcom,
+  US01,
+  US03,
+  US04,
+  US11,
+  US12,
+  US13,
+  US14,
+  US25,
+  US27,
+  US35,
+  US36,
+} = require("./gedcomParser");
 
 test("Testing Base Case", () => {
   expect(parseGedcom("gedcomTestDataBase").noError).toBe(true);
@@ -12,6 +25,14 @@ test("Test US01", () => {
   expect(US01("gedcomTestData_US01")).toBe(false);
 });
 
+test("Test US03", () => {
+  expect(US03("gedcomTestData_US03")).toBe(false);
+});
+
+test("Test US04", () => {
+  expect(US04("gedcomTestData_US04")).toBe(false);
+});
+
 test("Test US11", () => {
   expect(US11("gedcomTestData_US11")).toBe(false);
 });
@@ -20,10 +41,26 @@ test("Test US12", () => {
   expect(US12("gedcomTestData_US12")).toBe(false);
 });
 
+test("Test US13", () => {
+  expect(US13("gedcomTestData_US13")).toBe(false);
+});
+
+test("Test US14", () => {
+  expect(US14("")).toBe(false);
+});
+
 test("Test US25", () => {
   expect(US25("")).toBe(false);
 });
 
 test("Test US27", () => {
   expect(US27("")).toBe(false);
+});
+
+test("Test US35", () => {
+  expect(US35("")).toBe(false);
+});
+
+test("Test US36", () => {
+  expect(US36("")).toBe(false);
 });
